340e1932c082d136c9c8661c59d16cd5a480fc05
zhenxianluo <chinaitlearner@gmail.com>
2017-08-11 07:24:30 +0800

    update

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #coding:utf-8
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
 import ipdb, json, time, requests, os
 from dotenv import load_dotenv, find_dotenv
-# 自动查找并加载.env文件里的环境变量
 load_dotenv(find_dotenv())
 #from friends import friends
 #from bs4 import BeautifulSoup as BS
@@ -12,7 +12,7 @@ prefs = {"profile.managed_default_content_settings.images":2}
 chromeOptions.add_experimental_option("prefs",prefs)
 #driver = webdriver.Chrome(executable_path=r'/usr/local/bin/chromedriver',\
 #                          chrome_options=chromeOptions)
-driver = webdriver.Chrome(executable_path=r'/usr/bin/chromedriver',\
+driver = webdriver.Chrome(executable_path=os.environ.get('chrome_path'),\
                           chrome_options=chromeOptions)
 driver.maximize_window()
 cookie_file = 'website_cookie.json'
@@ -26,6 +26,7 @@ if os.path.exists(cookie_file):
             for item in json.loads(cookie_read):
                 driver.add_cookie(item)
             driver.refresh()
+            time.sleep(5)
 if driver.current_url == base_url:
     driver.delete_all_cookies()
     # begin login
@@ -36,6 +37,7 @@ if driver.current_url == base_url:
     driver.find_element_by_id('p').send_keys(os.environ.get('num_p'))
     driver.find_element_by_id('login_button').send_keys(Keys.ENTER)
     # end
+    while driver.current_url == base_url: time.sleep(1)
     # begin save cookie
     cookie_str = json.dumps(driver.get_cookies())
     open('website_cookie.json', 'ab').write(cookie_str)
@@ -51,13 +53,16 @@ if(document.documentElement.scrollHeight == document.documentElement.clientHeigh
     document.getElementsByTagName('body')[0].setAttribute('haha', 'blow')
 }
 """
+screen_origin = "document.getElementsByTagName('body')[0].setAttribute('haha', 'origin')"
 driver.get(url + user)
-user_dir = root_dir+os.sep+'rxx'
+user_dir = root_dir + os.sep + driver.find_elements_by_class_name('textoverflow')[1].text
 if not os.path.exists(user_dir):
     os.mkdir(user_dir)
 btn_sure = driver.find_elements_by_class_name('btn-fs-sure')
 if len(btn_sure) > 0: btn_sure[0].click()
-driver.find_elements_by_class_name('menu_item_4')[1].click() #点击进入好友相册
+el =  WebDriverWait(driver, 10).until(lambda x: x.find_elements_by_class_name('menu_item_4'))
+el[1].click()
+#driver.find_elements_by_class_name('menu_item_4')[1].click() #点击进入好友相册
 sub_frame = driver.find_element_by_id("tphoto")
 driver.switch_to_frame(sub_frame)
 select_ph = 0
@@ -70,17 +75,19 @@ while select_ph < len(driver.find_elements_by_class_name('js-album-cover')):
     while driver.find_element_by_tag_name('body').get_attribute('haha') != 'blow':
         driver.execute_script("window.scrollBy(0,500)")
         driver.execute_script(screen_blow)
+        time.sleep(0.5)
+    driver.execute_script(screen_origin)
     driver.execute_script("window.scrollTo(0,0)")
     driver.switch_to_frame(sub_frame)
     imgs = driver.find_elements_by_class_name('j-pl-photoitem-img')
     for i, img in enumerate(imgs):
         try:
-            r = requests.get(img.get_attribute('src').replace('/m/', '/b/'))
+            #r = requests.get(img.get_attribute('src').replace('/m/', '/b/'))
         except Exception, e:
             print i, 'error'
             continue
         if r.status_code == 200:
-            open(user_dir+os.sep+str(i)+'.jpg', 'wb').write(r.content)
+            #open(user_dir+os.sep+str(i)+'.jpg', 'wb').write(r.content)
             i = i + 1
             print "you are download the %d photo, and also %d" % (i, len(imgs)-i)
     driver.find_elements_by_class_name('js-select')[0].click() #点击进入好友相册
